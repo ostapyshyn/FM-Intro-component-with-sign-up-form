59fb674d55c0a2b5b8a4ac36b7ecc970848259f0
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-29 11:49:17 +0300

    Add basic styles

---
+++ src/components/InfoBlock/InfoBlock.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './InfoBlock.module.scss';
+
+const InfoBlock = () => {
+  return (
+    <section>
+      <h1>Learn to code by watching others</h1>
+      <p>
+        See how experienced developers solve problems in real-time. Watching scripted tutorials is
+        great, but understanding how developers think is invaluable.
+      </p>
+    </section>
+  );
+};
+
+export default InfoBlock;
